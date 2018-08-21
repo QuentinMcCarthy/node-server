7a25450d334210ed9bf03d97d67220783d6e8471
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-22 10:40:29 +1200

    Revert to console logging

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,20 @@
 console.log("JS is loaded into the page");
 
-$.ajax({
-	type: "GET",
-	url: "http://192.168.33.10:3000/allProducts",
-	dataType: "json",
-	success: function(products){
-		// console.log(products);
 
-		for(var i = 0; i < products.length; i++){
-			$("#allProducts").append(
-				$("<span>").text("["+products[i].id+"]:["+products[i].product+"]:["+products[i].price+"]:["+products[i].inStock+"]")
-			).append(
-				$("<br>")
-			);
+function getAllProducts(){
+	$.ajax({
+		type: "GET",
+		url: "http://192.168.33.10:3000/allProducts",
+		dataType: "json",
+		success: function(products){
+			console.log(products);
+		},
+		error: function(err){
+			console.log("Error "+err.status);
+			console.log(err);
 		}
-	},
-	error: function(err){
-		console.log("Error "+err.status);
-		console.log(err);
-	}
-});
+	});
+}
 
 function getInStockProducts(){
 	$.ajax({
